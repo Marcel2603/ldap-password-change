b7c812ec6f6b57e1a9bde8e8a8ca99b6e9ad1787
Marcel Herhold <herhold.marcel@gmail.com>
2024-12-20 19:37:31 +0100

    Fixing config

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -1,12 +1,12 @@
 package config
 
 type Config struct {
-	Server struct {
-		Port string `yaml:"port"`
-		Host string `yaml:"host"`
-	} `yaml:"server"`
-	Validation struct {
-		UsernamePattern string `yaml:"username" envconfig:"VALIDATION_USERNAME_PATTERN"`
-		PasswordPattern string `yaml:"password" envconfig:"VALIDATION_PASSWORD_PATTERN"`
-	} `yaml:"validation"`
+  Server struct {
+    Port string `yaml:"port"`
+    Host string `yaml:"host"`
+  } `yaml:"server"`
+  Validation struct {
+    UsernamePattern string `yaml:"username"`
+    PasswordPattern string `yaml:"password"`
+  } `yaml:"validation"`
 }
