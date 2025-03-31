99f1c18ac10b38d909f8489d8ce396e5cfcf7e10
Adrien Wehrung <adri.w51@hotmail.fr>
2025-03-31 10:44:02 +0200

    go fmt

--- wire.go
+++ wire.go
@@ -4,18 +4,18 @@
 package main
 
 import (
-  "github.com/google/wire"
-  "ldap-password-change/cmd/config"
-  "ldap-password-change/internal/service/ldap"
-  "ldap-password-change/internal/validation"
+	"github.com/google/wire"
+	"ldap-password-change/cmd/config"
+	"ldap-password-change/internal/service/ldap"
+	"ldap-password-change/internal/validation"
 )
 
 func InitializeService(ldapConfig config.LdapConfig, validationConfig config.ValidationConfig) (ldap.ServiceImpl, error) {
-  wire.Build(ldap.CreateService, ldap.CreateWrapper)
-  return ldap.ServiceImpl{}, nil
+	wire.Build(ldap.CreateService, ldap.CreateWrapper)
+	return ldap.ServiceImpl{}, nil
 }
 
 func InitializeValidator(validationConfig config.ValidationConfig) (validation.ValidatorImpl, error) {
-  wire.Build(validation.CreateValidator)
-  return validation.ValidatorImpl{}, nil
+	wire.Build(validation.CreateValidator)
+	return validation.ValidatorImpl{}, nil
 }
